e9a8be627a359fada0b79c4d2928bb338b88667e
Wolfhill09 <harvey.akhurst@icloud.com>
2022-08-01 19:03:27 +0100

    Tweak: Change vRP to be lower case

--- cratedrop/sv_cratedrop.lua
+++ cratedrop/sv_cratedrop.lua
@@ -1,5 +1,5 @@
-local Tunnel = module('vRP', 'lib/Tunnel')
-local Proxy = module('vRP', 'lib/Proxy')
+local Tunnel = module('vrp', 'lib/Tunnel')
+local Proxy = module('vrp', 'lib/Proxy')
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP", "vRP_gunshop")
 
